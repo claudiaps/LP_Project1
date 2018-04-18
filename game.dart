59e7d3468fe538087c42fcf0ebe72830c1504406
Unknown <clp.sampedro@gmail.com>
2018-04-18 16:56:02 -0300

    final bugs resolved

--- game.dart
+++ game.dart
@@ -21,14 +21,15 @@ class Game {
   initClass(String path) {
     initialPrint();
     String command = menu();
-    print(command);
+    command = command.toLowerCase();
     while (command != 'new' && command != 'load') {
       command =
           menu(); //Armazena a opção retornada da função "menu" do arquivo scenesControl
       if (command == 'help') {
         printHelp();
       } else {
-        print("\n Comando errado, tente new, load ou help\n");
+        print(
+            "\n Comando errado ou indisponível no momento, tente new, load ou help\n");
       }
     }
     new File(path)
@@ -68,7 +69,8 @@ class Game {
             read_line();
           }
           if (currentScene == 13) {
-            end_game();          }
+            end_game();
+          }
         }
       }
     });
@@ -78,11 +80,10 @@ class Game {
       stdout.write("Digite o nome do jogo salvo que deseja retomar:");
       stdout.write("/> ");
       String save = stdin.readLineSync();
-
-      new File('saves/$save.json')
+      var file = new File('saves/$save.json');
+      file
           .readAsString()
-          .then((fileContents) =>
-              JSON.decode(fileContents)) //Lê o arquivo com base no nome passado
+          .then((fileContents) => JSON.decode(fileContents))
           .then((jsonData) {
         currentScene = jsonData["cenaAtual"];
         inventory = jsonData["inventario"];
@@ -110,8 +111,15 @@ class Game {
             end_game();
           }
         }
-      });
+      }).catchError((e) {
+        print("Arquivo com nome errado. Inicie o jogo de novo, e digite o nome do save correto");
+        return;
+      }); //Lê o arquivo com base no nome passado
     }
+  }
+
+  handleError() {
+    print("Nome do arquivo incorreto!");
   }
 
 /*Função que faz a verificação dos comandos passados pelo usuário*/
@@ -123,18 +131,20 @@ class Game {
     var texto = new List();
     texto = text.split(" ");
 
-    if (texto[0] == 'help') {
-      printHelp();
-      return;
-    } else if (texto[0] == 'exit') {
-      exit_game();
-      return;
-    } else if (texto[0] == 'inventory') {
-      inventory_command();
-      return;
-    } else if (texto[0] == 'save') {
-      save_command();
-      return;
+    if (texto.length == 1) {
+      if (texto[0] == 'help') {
+        printHelp();
+        return;
+      } else if (texto[0] == 'exit') {
+        exit_game();
+        return;
+      } else if (texto[0] == 'inventory') {
+        inventory_command();
+        return;
+      } else if (texto[0] == 'save') {
+        save_command();
+        return;
+      }
     }
     if (texto.length == 2 || texto.length == 4) {
       //Verifica se é "use OBJETO" ou "use OBJETO1 WITH OBJETO2"
@@ -170,7 +180,7 @@ class Game {
         }
       }
     }
-    print('Comando errado, tenta outro');
+    print('Comando errado, tente outro!');
   }
 
   /*Função para exibir a descrição da cena*/
@@ -191,7 +201,7 @@ class Game {
           } else {
             inventory.add(item);
             object.got = true;
-            print("$item adicionado ao inventário\n");
+            print("$item adicionado ao inventário");
             return;
           }
         }
@@ -309,7 +319,7 @@ class Game {
   /*Função para exibir a ajuda*/
   printHelp() {
     print(
-        "Para jogar Bill's Journey, basta associar os objetos certos com cada cena ou objetos do seu inventário com a cena.\nOs comandos são: \n  USE: Usar um objeto \n  GET: Colocar um objeto em seu inventário \n  DESCRIPTION: Descrição do objeto \n  USE ___ WITH ____: Usar um objeto do inventário com um da cena \n INVENTORY: Exibir itens do inventário \n  EXIT: Sair do jogo \n  SAVE: Salvar o jogo \n");
+        "\nPara jogar Bill's Journey, basta associar os objetos certos com cada cena ou objetos do seu inventário com a cena.\nOs comandos para jogar são: \n\n  USE: Usar um objeto \n  GET: Colocar um objeto em seu inventário \n  DESCRIPTION: Descrição do objeto \n  USE ___ WITH ____: Usar um objeto do inventário com um da cena \n INVENTORY: Exibir itens do inventário \n  EXIT: Sair do jogo \n  SAVE: Salvar o jogo \n\nAo iniciar o jogo, você tem algumas opições: Iniciar um jogo novo (comando NEW) ou carregar um jogo já salvo (comando LOAD). A função de LOAD só está disponível no início do jogo");
   }
 
   /*Função que controla a desistência no jogo*/
@@ -331,7 +341,7 @@ class Game {
         '\n Essa história conta sobre acasos da vida de um jovem estudante universitário chamado Bill. Ele, como todo universitário, passa por vários perrengues durante sua vida acadêmica: provas, trabalhos, vida longe da família, etc.  Bill estava repleto de provas e trabalhos, devido ao final do seu primeiro semestre na faculdade. Professores não paravam de passar novos trabalhos, os já existentes estavam atrasados, provas complicadas estavam chegando. Ele precisava ficar acordado...\n (aaaah pequeno detalhe. AS frases em parênteses são pensamentos do narrador, ou programador no caso. Vai que a ideia fica legal) \n\n');
   }
 
-  end_game(){
+  end_game() {
     print("Obrigada por jogar e tentar entender a história!");
     exit(0);
   }
